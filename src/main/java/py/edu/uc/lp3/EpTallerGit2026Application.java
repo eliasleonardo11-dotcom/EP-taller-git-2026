ca2039db05fadabae3068ce71544673ae0569f94
eliasleonardo11-dotcom <eliasleonardo11@gmail.com>
2026-09-23 13:20:33 -0300

    Add files via upload

---
+++ src/main/java/py/edu/uc/lp3/EpTallerGit2026Application.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpTallerGit2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpTallerGit2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/py/edu/uc/lp3/EpTallerGit2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpTallerGit2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
